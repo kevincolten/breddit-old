81d854699f7eeee99c4643a819f96ce18010b805
mistakevin <kevincolten@gmail.com>
2015-11-30 19:49:14 -0600

    auth complete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    if (Auth::guest()) {
+        return Redirect::to('auth/login');
+    } else {
+        echo 'Hello ' . Auth::user()->email;
+    }
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
